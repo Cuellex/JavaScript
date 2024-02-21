e99572aa7681e6d2b713420deaee262767bbd981
Santiago Cuello <153139534+Cuellex@users.noreply.github.com>
2024-02-21 15:40:20 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 let costeTotal =  0;
+let confirmacion = 0;
 
 function solicitarNochesHotel() {
     let noches = prompt("Ingresa el número de noches (3-7):");
     if (!isNaN(noches)) {
         switch(noches) {
             case "3":
-                alert("El costo por 3 noches es $USD350");
+                alert("El costo por 3 noches es $USD200");
                 costeTotal += 200;
                 break;
 
             case "4":
-                alert("El costo por 4 noches es $USD350");
+                alert("El costo por 4 noches es $USD250");
                 costeTotal += 250;
                 break;
 
             case "5":
-                alert("El costo por 5 noches es $USD350");
+                alert("El costo por 5 noches es $USD300");
                 costeTotal += 300;
                 break;
 
@@ -25,7 +26,7 @@ function solicitarNochesHotel() {
                 break;
 
             case "7":
-                alert("El costo por 7 noches es $USD350");
+                alert("El costo por 7 noches es $USD400");
                 costeTotal += 400;
                 break;
 
@@ -43,27 +44,27 @@ function solicitarVuelo() {
     switch(ciudad) {
         case "Madrid": // Buscar como corregir si alguien escribe con errores de ortografia
             alert("El costo del vuelo a Madrid es $USD110");
-            costeTotal += 130;
+            costeTotal += 110;
             break;
 
         case "Paris":
-            alert("El costo del vuelo a Madrid es $USD140");
-            costeTotal += 130;
+            alert("El costo del vuelo a Paris es $USD140");
+            costeTotal += 140;
             break;
 
         case "Roma":
-            alert("El costo del vuelo a Madrid es $USD100");
-            costeTotal += 130;
+            alert("El costo del vuelo a Roma es $USD100");
+            costeTotal += 100;
             break;
 
         case "Moscu":
-            alert("El costo del vuelo a Madrid es $USD125");
-            costeTotal += 130;
+            alert("El costo del vuelo a Moscu es $USD125");
+            costeTotal += 125;
             break;
 
         case "Bs As":
-            alert("El costo del vuelo a Madrid es $USD90");
-            costeTotal += 130;
+            alert("El costo del vuelo a Buenos Aires es $USD90");
+            costeTotal += 90;
             break;
 
         default:
@@ -77,28 +78,28 @@ function solicitarAlquilerCoche() {
     if (!isNaN(dias)) {
         switch(dias) {
             case "3":
-                alert("El costo del alquiler del coche por 5 días es $USD200");
-                costeTotal +=  400;
+                alert("El costo del alquiler del coche por 3 días es $USD200");
+                costeTotal += 200;
                 break;
 
             case "4":
-                alert("El costo del alquiler del coche por 5 días es $USD225");
-                costeTotal +=  400;
+                alert("El costo del alquiler del coche por 4 días es $USD225");
+                costeTotal += 225;
                 break;
 
             case "5":
                 alert("El costo del alquiler del coche por 5 días es $USD250");
-                costeTotal +=  400;
+                costeTotal += 250;
                 break;
             
             case "6":
-                alert("El costo del alquiler del coche por 5 días es $USD275");
-                costeTotal +=  400;
+                alert("El costo del alquiler del coche por 6 días es $USD275");
+                costeTotal += 275;
                 break;
 
             case "7":
-                alert("El costo del alquiler del coche por 5 días es $USD300");
-                costeTotal +=  400;
+                alert("El costo del alquiler del coche por 7 días es $USD300");
+                costeTotal += 300;
                 break;
 
             default:
@@ -111,14 +112,37 @@ function solicitarAlquilerCoche() {
 }
 
 function mostrarResumenYCostoTotal() {
-    alert(`Tu presupuesto final es de: $${costeTotal}`);
+    alert(`Tu presupuesto final es de: $${costeTotal}.`);
+}
+
+function confirmacionDeGastos(){
+    let confirmar = prompt("¿Quiere hacer alguna modificación? (Si/No)");
+        switch(confirmar){
+            case 'No':
+                confirmacion = 0;
+            break;
+
+            case 'Si':
+                confirmacion = 1;
+            break;
+
+            default:
+            alert("Opción no disponible.");
+            break;
+        }
+    
 }
 
 function iniciarCalculo() {
-    solicitarNochesHotel();
-    solicitarVuelo();
-    solicitarAlquilerCoche();
-    mostrarResumenYCostoTotal();
+    confirmacion = 1;
+    while(confirmacion == 1){
+        solicitarNochesHotel();
+        solicitarVuelo();
+        solicitarAlquilerCoche();
+        mostrarResumenYCostoTotal();
+        confirmacionDeGastos();
+        costeTotal = 0;
+    }
 }
 
 iniciarCalculo();
